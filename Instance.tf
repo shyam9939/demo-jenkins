a6a426ac027f0aa72ff00278ba52a0c44cd4c54c
shyam9939 <147368372+shyam9939@users.noreply.github.com>
2025-02-01 00:50:25 +0530

    Create Instance.tf

---
+++ Instance.tf
@@ -0,0 +1,46 @@
+# main.tf
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+# Configure the Google Cloud provider
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+# Example: Create a Google Compute Engine instance
+resource "google_compute_instance" "example_instance" {
+  name         = "example-instance"
+  machine_type = "e2-medium"
+  zone         = "${var.region}-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      # Ephemeral IP
+    }
+  }
+  labels = {
+    environment = "development"
+    managed_by  = "terraform"
+  }
+}
+# variables.tf
+variable "project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+  default     = "shrameshwar"
+}
+variable "region" {
+  description = "The GCP region for resources"
+  type        = string
+  default     = "us-central1"
+}
